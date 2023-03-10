c38865110f259b82bcc04fb409762eed8cac2b3a
Gabriel <gabrielsantos@grsnucleo.com.br>
2023-03-10 11:09:21 -0300

    Create job_inserir_funcionarios.py

---
+++ scripts/jobs/job_inserir_funcionarios.py
@@ -0,0 +1,60 @@
+if __name__ == '__main__':
+    import sys
+    sys.path.append('../GRS_Manager')
+
+    from datetime import datetime
+    from os.path import basename
+
+    from manager import TIMEZONE_SAO_PAULO, database
+    from manager.email_connect import EmailConnect
+    from manager.models import Empresa, EmpresaPrincipal, Funcionario
+
+
+    EMPRESAS_PRINCIPAIS: list[EmpresaPrincipal]  = EmpresaPrincipal.query.all()
+    TENTATIVAS = 4
+
+    NOME_TABELA = 'Funcionarios'
+    ACAO = 'Inserir'
+
+
+    total_inseridos = 0
+    erro = None
+
+    for empresa_principal in EMPRESAS_PRINCIPAIS:
+        empresas: list[Empresa] = (
+            database.session.query(Empresa.id_empresa)
+            .filter(Empresa.cod_empresa_principal == empresa_principal.cod)
+            .all()
+        )
+        empresas = [i.id_empresa for i in empresas]
+        for i in range(TENTATIVAS):
+            print(f'{empresa_principal.nome} - {ACAO} {NOME_TABELA} - tentativa: {i + 1}/{TENTATIVAS}', end=' - ')
+            try:
+                qtd_inseridos = Funcionario.inserir_funcionarios(cod_empresa_principal=empresa_principal.cod, lista_empresas=empresas)
+                print(f'Qtd: {qtd_inseridos}')
+                total_inseridos = total_inseridos + qtd_inseridos
+                break
+            except Exception as exception:
+                erro = type(exception).__name__
+                print(f'ERRO: {erro}')
+                continue
+
+
+    # enviar report
+    DESTINATARIO = ['gabrielsantos@grsnucleo.com.br']
+    TIMESTAMP = datetime.now(tz=TIMEZONE_SAO_PAULO).strftime('%d-%m-%Y %H:%M:%S')
+    FILENAME = basename(__file__)
+
+    print(f'Enviando report...')
+    enviar_report = EmailConnect.send_email(
+        to_addr = DESTINATARIO,
+        message_subject = f"{FILENAME} executado {TIMESTAMP}",
+        message_body = str({NOME_TABELA: total_inseridos, 'Erro': erro})
+    )
+    if enviar_report['sent'] == 1:
+        print(f'Enviado para: {DESTINATARIO}')
+    else:
+        print(f"Não enviado. ERRO: {enviar_report['error']}")
+    
+    print(f'Done! Total: {total_inseridos}')
+
