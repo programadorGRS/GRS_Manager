e69832fc32aa9c0da612a8d7ccab70c016fb4d21
Gabriel <gabrielsantos@grsnucleo.com.br>
2023-03-10 11:09:52 -0300

    Update jobs_atualizar.py
    atualizado envio de report ao terminar o script

--- scripts/jobs/jobs_atualizar.py
+++ scripts/jobs/jobs_atualizar.py
@@ -8,9 +8,15 @@ if __name__ == '__main__':
     sys.path.append('../GRS_Manager')
 
     from datetime import datetime, timedelta
-    from manager.email import enviar_report
-    from manager.models import Pedido, Empresa, Unidade, EmpresaPrincipal, Prestador, Exame
+    from smtplib import SMTPException
 
+    from manager import TIMEZONE_SAO_PAULO
+    from manager.email_connect import EmailConnect
+    from manager.models import (Empresa, EmpresaPrincipal, Exame, Pedido,
+                                Prestador, Unidade)
+
+
+    DIAS_PASSADO = 10
 
     total_empresas = 0
     total_unidades = 0
@@ -39,8 +45,7 @@ if __name__ == '__main__':
             tags = Pedido.atualizar_tags_prev_liberacao() 
             total_tags = total_tags + tags
 
-            # atualizar nos ultimos 90 dias
-            data_inicio = datetime.now() - timedelta(days=90)
+            data_inicio = datetime.now() - timedelta(days=DIAS_PASSADO)
             data_fim = data_inicio + timedelta(days=31)
             while data_inicio <= datetime.now():
                 pedidos = Pedido.atualizar_pedidos(
@@ -55,17 +60,23 @@ if __name__ == '__main__':
                 total_pedidos = total_pedidos + pedidos
 
 
-
     print('Enviando report...')
-    enviar_report(
-        subj='jobs_atualizar executado',
-        send_to=['gabrielsantos@grsnucleo.com.br'],
-        body=str({
-            'Empresas': total_empresas,
-            'Unidades': total_unidades,
-            'Prestadores': total_prestadores,
-            'Exames': total_exames,
-            'TagsPedidos': total_tags,
-            'Pedidos:': total_pedidos
-        })
-    )
+    time_stamp = datetime.now(tz=TIMEZONE_SAO_PAULO).strftime('%d-%m-%Y %H:%M:%S')
+    for i in range(3):
+        try:
+            EmailConnect.send_email(
+                to_addr = ['gabrielsantos@grsnucleo.com.br'],
+                message_subject = f"jobs_atualizar executado {time_stamp}",
+                message_body = str({
+                    'Empresas': total_empresas,
+                    'Unidades': total_unidades,
+                    'Prestadores': total_prestadores,
+                    'Exames': total_exames,
+                    'TagsPedidos': total_tags,
+                    'Pedidos:': total_pedidos
+                })
+            )
+            break
+        except SMTPException:
+            continue
+
