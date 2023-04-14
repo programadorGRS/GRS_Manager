c3ca270ba609ef856d9b4100ef3f4ab8f12d3fb9
Gabriel <gabrielsantos@grsnucleo.com.br>
2023-04-14 14:24:04 -0300

    added dataclass InfosCarregar

---
+++ src/main/job/infos_carregar.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from datetime import datetime
+from src import TIMEZONE_SAO_PAULO
+
+@dataclass
+class InfosCarregar:
+    '''
+        Dataclass para coletar informações sobre os \
+        Jobs de carregameto em geral
+    '''
+    tabela: str
+    qtd_inseridos: int = 0
+    qtd_atualizados: int = 0
+    ok: bool = True
+    erro: str | None = None
+    data_hora: datetime = datetime.now(TIMEZONE_SAO_PAULO)
+
