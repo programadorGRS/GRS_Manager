e4264df7e52aff24a672b2b5aa66fcef0fbe3bbe
Gabriel <gabrielsantos@grsnucleo.com.br>
2023-04-21 17:34:46 -0300

    removed send report from main job scripts
    terrible idea

--- scripts/jobs/main/job_atualizar_prestadores.py
+++ scripts/jobs/main/job_atualizar_prestadores.py
@@ -2,11 +2,7 @@ if __name__ == '__main__':
     import sys
     sys.path.append('../GRS_Manager')
 
-    from datetime import datetime
-    from os.path import basename
-
-    from src import TIMEZONE_SAO_PAULO, app
-    from src.email_connect import EmailConnect
+    from src import app
     from src.main.empresa_principal.empresa_principal import EmpresaPrincipal
     from src.main.prestador.prestador import Prestador
 
@@ -32,22 +28,5 @@ if __name__ == '__main__':
                 print(f'ERRO: {erro}')
                 continue
 
-
-        # enviar report
-        DESTINATARIO = ['gabrielsantos@grsnucleo.com.br']
-        TIMESTAMP = datetime.now(tz=TIMEZONE_SAO_PAULO).strftime('%d-%m-%Y %H:%M:%S')
-        FILENAME = basename(__file__)
-
-        print(f'Enviando report...')
-        enviar_report = EmailConnect.send_email(
-            to_addr = DESTINATARIO,
-            message_subject = f"{FILENAME} executado {TIMESTAMP}",
-            message_body = str({'Prestadores': total_inseridos, 'Erro': erro})
-        )
-        if enviar_report['sent'] == 1:
-            print(f'Enviado para: {DESTINATARIO}')
-        else:
-            print(f"Não enviado. ERRO: {enviar_report['error']}")
-        
         print(f'Done! Total: {total_inseridos}')
 
