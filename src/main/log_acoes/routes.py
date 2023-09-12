22f33b414dc2ac83169ea46abacf8bd2a075e7a1
Gabriel <gabrielsantos@grsnucleo.com.br>
2023-09-12 16:17:10 -0300

    refactored log acoes to blueprint

--- src/main/log_acoes/routes.py
+++ src/main/log_acoes/routes.py
@@ -1,38 +1,42 @@
-import datetime as dt
+import os
+from datetime import datetime
 
 import pandas as pd
-from flask import render_template, send_from_directory
+from flask import (Blueprint, flash, redirect, render_template,
+                   send_from_directory, url_for)
 from flask_login import login_required
 
-from src import UPLOAD_FOLDER, app, database
-from src.main.log_acoes.log_acoes import LogAcoes
-from src.utils import admin_required
+from src import UPLOAD_FOLDER
+from src.extensions import database as db
 
 from .forms import FormLogAcoes
+from .log_acoes import LogAcoes
+
+log_acoes_bp = Blueprint(
+    name="log_acoes",
+    import_name=__name__,
+    url_prefix="/log-acoes",
+    template_folder="templates",
+)
 
 
-@app.route('/log_acoes', methods=['GET', 'POST'])
+@log_acoes_bp.route("/", methods=["GET", "POST"])
 @login_required
-@admin_required
 def log_acoes():
     form = FormLogAcoes()
-    
-    form.tabela.choices = (
-        [('', 'Selecione')] + (
-            LogAcoes.query.with_entities(LogAcoes.tabela, LogAcoes.tabela)
-            .order_by(LogAcoes.tabela)
-            .distinct()
-            .all()
-        )
+
+    form.tabela.choices = [("", "Selecione")] + (
+        db.session.query(LogAcoes.tabela, LogAcoes.tabela)  # type: ignore
+        .order_by(LogAcoes.tabela)
+        .distinct()
+        .all()
     )
-    
-    form.usuario.choices = (
-        [('', 'Selecione')] + (
-            LogAcoes.query.with_entities(LogAcoes.id_usuario, LogAcoes.username)
-            .order_by(LogAcoes.username)
-            .distinct()
-            .all()
-        )
+
+    form.usuario.choices = [("", "Selecione")] + (
+        db.session.query(LogAcoes.id_usuario, LogAcoes.username)  # type: ignore
+        .order_by(LogAcoes.username)
+        .distinct()
+        .all()
     )
 
     if form.validate_on_submit():
@@ -40,23 +44,26 @@ def log_acoes():
             inicio=form.data_inicio.data,
             fim=form.data_fim.data,
             usuario=form.usuario.data,
-            tabela=form.tabela.data
+            tabela=form.tabela.data,
         )
 
-        df = pd.read_sql(sql=query.statement, con=database.session.bind)
-        
+        df = pd.read_sql(sql=query.statement, con=db.session.bind)  # type: ignore
+
+        if df.empty:
+            flash("Sem dados", "alert-info")
+            return redirect(url_for("log_acoes.log_acoes"))
+
         # remover decimais da hora
-        df['hora'] = list(map(lambda x: str(x).split('.')[0], df['hora']))
-        
-        # criar csv dentro da pasta
-        nome_arqv = f'Log_acoes_{int(dt.datetime.now().timestamp())}.csv'
-        camihno_arqv = f'{UPLOAD_FOLDER}/{nome_arqv}'
-        df.to_csv(
-            camihno_arqv,
-            sep=';',
-            index=False,
-            encoding='iso-8859-1'
+        df["hora"] = list(map(lambda x: str(x).split(".")[0], df["hora"]))
+
+        timestamp = int(datetime.now().timestamp())
+        nome_arqv = f"Log_acoes_{timestamp}.csv"
+
+        camihno_arqv = os.path.join(UPLOAD_FOLDER, nome_arqv)
+        df.to_csv(camihno_arqv, sep=";", index=False, encoding="iso-8859-1")
+
+        return send_from_directory(
+            directory=UPLOAD_FOLDER, path="/", filename=nome_arqv
         )
-        return send_from_directory(directory=UPLOAD_FOLDER, path='/', filename=nome_arqv)
-    
-    return render_template('log_acoes.html', title='GRS+Connect', form=form)
+
+    return render_template("log_acoes/log_acoes.html", form=form)
