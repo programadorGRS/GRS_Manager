e411ee4e469871e207f866ade326a3a1db2e7876
Gabriel <gabrielsantos@grsnucleo.com.br>
2023-09-12 16:24:55 -0300

    refactored route refs to register new blueprints

--- src/_routerefs.py
+++ src/_routerefs.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_limiter.errors import RateLimitExceeded
 from werkzeug.exceptions import HTTPException
 
-from src.api import routes
+from src.api.routes import api_bp
 from src.api.routes_internas import internal_api
 from src.main.absenteismo.routes import absenteismo_bp
 from src.main.central_avisos import central_avisos
@@ -14,16 +14,16 @@ from src.main.error_handlers.error_handlers import (error_404, http_exceptions,
                                                     rate_limit_exceptions)
 from src.main.exame import exame_bp
 from src.main.exames_realizados.routes import exames_realizados_bp
-from src.main.grupo import routes
-from src.main.home import routes
-from src.main.importacao import routes
-from src.main.log_acoes import routes
+from src.main.grupo.routes import grupo_bp
+from src.main.home.routes import home_bp
+from src.main.importacao.routes import importacao_bp
+from src.main.log_acoes.routes import log_acoes_bp
 from src.main.login.routes import user_auth
-from src.main.manual import routes
+from src.main.manual.routes import manual_bp
 from src.main.pedido import routes
 from src.main.pedido_socnet import routes
 from src.main.prestador import prestador_bp
-from src.main.relatorios_agendados import routes
+from src.main.relatorios_agendados.routes import rel_agendados_bp
 from src.main.rtc.routes import rtc
 from src.main.status import status_bp
 from src.main.unidade import unidade
@@ -32,20 +32,32 @@ from src.main.usuario.routes import usuario_bp
 
 def register_blueprints(app: Flask):
     """Registers all Blueprints"""
-    app.register_blueprint(central_avisos)
-    app.register_blueprint(empresa)
-    app.register_blueprint(empresa_socnet)
-    app.register_blueprint(internal_api)
-    app.register_blueprint(user_auth)
-    app.register_blueprint(conv_exames)
-    app.register_blueprint(rtc)
-    app.register_blueprint(unidade)
-    app.register_blueprint(status_bp)
-    app.register_blueprint(absenteismo_bp)
-    app.register_blueprint(exame_bp)
-    app.register_blueprint(usuario_bp)
-    app.register_blueprint(exames_realizados_bp)
-    app.register_blueprint(prestador_bp)
+    BPS = [
+        central_avisos,
+        empresa,
+        empresa_socnet,
+        internal_api,
+        user_auth,
+        conv_exames,
+        rtc,
+        unidade,
+        status_bp,
+        absenteismo_bp,
+        exame_bp,
+        usuario_bp,
+        exames_realizados_bp,
+        prestador_bp,
+        home_bp,
+        log_acoes_bp,
+        manual_bp,
+        rel_agendados_bp,
+        api_bp,
+        grupo_bp,
+        importacao_bp,
+    ]
+
+    for bp in BPS:
+        app.register_blueprint(bp)
     return app
 
 
