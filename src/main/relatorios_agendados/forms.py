89f123ae4fa8f465801449d0d3ab1c58bb40569b
Gabriel <gabrielsantos@grsnucleo.com.br>
2023-09-12 16:18:51 -0300

    refactored relatorios agendados to blueprint

--- src/main/relatorios_agendados/forms.py
+++ src/main/relatorios_agendados/forms.py
@@ -1,28 +1,24 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileField, FileRequired
-from wtforms import BooleanField, FileField, SelectField, SubmitField
+from wtforms import BooleanField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class FormConfigRelatoriosAgendados(FlaskForm):
     tabela = SelectField(
-        'Tabela para Atualizar',
-        choices=[
-            ('', 'Selecione'),
-            (1, 'Empresas'),
-            (2, 'Unidades')
-        ],
-        validators=[DataRequired()]
+        "Tabela para Atualizar",
+        choices=[("", "Selecione"), (1, "Empresas"), (2, "Unidades")],
+        validators=[DataRequired()],
+        coerce=lambda tab: int(tab) if tab else None,  # type: ignore
     )
     celulas_null = BooleanField(
-        'Excluir informações (excluir dados no banco de dados onde a celula no CSV estiver em branco)'
+        "Excluir informações (excluir dados no banco de dados onde a celula no CSV estiver em branco)"
     )
     csv = FileField(
-        'Escolher arquivo',
+        "Escolher arquivo",
         validators=[
             FileRequired(),
-            FileAllowed(['csv'], message='Apenas arquivos CSV')
-        ]
+            FileAllowed(["csv"], message="Apenas arquivos CSV"),
+        ],
     )
-    btn_upload = SubmitField('Upload')
-
+    btn_upload = SubmitField("Upload")
--- src/main/relatorios_agendados/routes.py
+++ src/main/relatorios_agendados/routes.py
@@ -1,228 +1,308 @@
-import datetime as dt
+import os
+from datetime import datetime
 from io import StringIO
 from sys import getsizeof
+from typing import Any
 
 import numpy as np
 import pandas as pd
-from flask import (flash, redirect, render_template, request,
+from flask import (Blueprint, flash, redirect, render_template, request,
                    send_from_directory, url_for)
 from flask_login import current_user, login_required
+from sqlalchemy.exc import DatabaseError, SQLAlchemyError
 
-from src import TIMEZONE_SAO_PAULO, UPLOAD_FOLDER, app, database
-from src.main.empresa.empresa import Empresa
-from src.main.unidade.unidade import Unidade
+from src import TIMEZONE_SAO_PAULO, UPLOAD_FOLDER, app
+from src.extensions import database as db
 from src.utils import admin_required, tratar_emails, zipar_arquivos
 
+from ..empresa.empresa import Empresa
+from ..unidade.unidade import Unidade
 from .forms import FormConfigRelatoriosAgendados
 
+rel_agendados_bp = Blueprint(
+    name="rel_agendados",
+    import_name=__name__,
+    url_prefix="/rel-agendados",
+    template_folder="templates",
+)
 
-@app.route('/relatorios_agendados/config', methods=['GET', 'POST'])
+
+@rel_agendados_bp.route("/config", methods=["GET", "POST"])
 @login_required
 @admin_required
-def config_relatorios_agendados():
+def config_rel_agendados():
     form = FormConfigRelatoriosAgendados()
 
-    if form.validate_on_submit() and 'btn_upload' in request.form:
+    if form.validate_on_submit():
         # ler arquivo
-        arqv = request.files['csv']
-        data = arqv.read().decode('iso-8859-1')
+        arqv = request.files["csv"]
+        data = arqv.read().decode("iso-8859-1")
 
         # tamnho maximo
-        max_size_mb = app.config['MAX_UPLOAD_SIZE_MB']
+        max_size_mb = app.config["MAX_UPLOAD_SIZE_MB"]
         max_bytes = max_size_mb * 1024 * 1024
 
         if getsizeof(data) > max_bytes:
-            flash(f'O arquivo deve ser menor que {max_size_mb} MB', 'alert-danger')
-            return redirect(url_for('importar_dados'))
-        else:
-            # ler string como objeto para csv
-            df: pd.DataFrame = pd.read_csv(
-                filepath_or_buffer=StringIO(data),
-                sep=';',
-                encoding='iso-8859-1',
+            flash(f"O arquivo deve ser menor que {max_size_mb} MB", "alert-danger")
+            return redirect(url_for("rel_agendados.config_rel_agendados"))
+
+        # ler string como objeto para csv
+        df: pd.DataFrame = pd.read_csv(
+            filepath_or_buffer=StringIO(data),
+            sep=";",
+            encoding="iso-8859-1",
+        )
+
+        if df.empty:
+            flash("Arquivo vazio", "alert-danger")
+            return redirect(url_for("rel_agendados.config_rel_agendados"))
+
+        COLS_EMAIL = [
+            "conv_exames_emails",
+            "exames_realizados_emails",
+            "absenteismo_emails",
+        ]
+
+        COLS_BOOL = [
+            "conv_exames",
+            "exames_realizados",
+            "absenteismo",
+        ]
+
+        tabela = form.tabela.data
+        match tabela:
+            case 1:  # Empresas
+                tabela = Empresa
+                coluna_chave = "id_empresa"
+
+                # checar colunas
+                colunas_erro = []
+                for col in df.columns:
+                    if col not in COLS_BOOL + COLS_EMAIL + [coluna_chave]:
+                        colunas_erro.append(col)
+
+                if colunas_erro:
+                    flash(f"Colunas erradas: {colunas_erro}", "alert-danger")
+                    return redirect(url_for("rel_agendados.config_rel_agendados"))
+
+                modelos_query = [
+                    (Empresa.id_empresa),
+                    (Empresa.razao_social),
+                    (Empresa.conv_exames),
+                    (Empresa.conv_exames_emails),
+                    (Empresa.exames_realizados),
+                    (Empresa.exames_realizados_emails),
+                    (Empresa.absenteismo),
+                    (Empresa.absenteismo_emails),
+                ]
+
+                id_empresas = df[coluna_chave].drop_duplicates().astype(int).tolist()
+                filtros = [Empresa.id_empresa.in_(id_empresas)]
+
+            case 2:  # Unidades
+                tabela = Unidade
+                coluna_chave = "id_unidade"
+
+                # checar colunas
+                colunas_erro = []
+                for col in df.columns:
+                    if col not in COLS_BOOL + COLS_EMAIL + [coluna_chave]:
+                        colunas_erro.append(col)
+
+                if colunas_erro:
+                    flash(f"Colunas erradas: {colunas_erro}", "alert-danger")
+                    return redirect(url_for("rel_agendados.config_rel_agendados"))
+
+                modelos_query = [
+                    (Unidade.id_unidade),  # inserir nome da empresa
+                    (Unidade.nome_unidade),
+                    (Unidade.conv_exames),
+                    (Unidade.conv_exames_emails),
+                    (Unidade.exames_realizados),
+                    (Unidade.exames_realizados_emails),
+                    (Unidade.absenteismo),
+                    (Unidade.absenteismo_emails),
+                ]
+
+                id_unidades = df[coluna_chave].drop_duplicates().astype(int).tolist()
+                filtros = [Unidade.id_unidade.in_(id_unidades)]
+
+            case _:
+                return redirect(url_for("rel_agendados.config_rel_agendados"))
+
+        # tratar df
+        df = __tratar_df_config(
+            df=df, col_chave=coluna_chave, curr_username=current_user.username  # type: ignore
+        )
+
+        df_mappings = df.to_dict(orient="records")
+
+        final_mappings = __handle_celulas_null(
+            df_mapping=df_mappings,  # type: ignore
+            manter_celulas_null=form.celulas_null.data,
+        )
+
+        # backup antes de atualizar
+        arq_bk = __gerar_arq_estado_atual(
+            nome="backup",
+            cols_query=modelos_query,
+            filtros_query=filtros,
+        )
+
+        try:
+            db.session.bulk_update_mappings(tabela, final_mappings)  # type: ignore
+            db.session.commit()  # type: ignore
+        except (SQLAlchemyError, DatabaseError) as e:
+            db.session.rollback()  # type: ignore
+            app.logger.error(msg=e, exc_info=True)
+            flash("Erro interno ao atualizar tabela", "alert-danger")
+            return redirect(url_for("rel_agendados.config_rel_agendados"))
+
+        # gerar report das atualizacoes
+        arq_updates = __gerar_arq_estado_atual(
+            nome="alteracoes",
+            cols_query=modelos_query,
+            filtros_query=filtros,
+        )
+
+        timestamp = int(datetime.now().timestamp())
+        nome_pasta = os.path.join(UPLOAD_FOLDER, f"report_{timestamp}.zip")
+
+        caminho_arquivos = zipar_arquivos(
+            caminhos_arquivos=[arq_updates, arq_bk],
+            caminho_pasta_zip=nome_pasta,
+        )
+
+        return send_from_directory(
+            directory=UPLOAD_FOLDER,
+            path="/",
+            filename=os.path.basename(caminho_arquivos),
+        )
+
+    return render_template("relatorios_agendados/config.html", form=form)
+
+
+def __tratar_df_config(df: pd.DataFrame, col_chave: str, curr_username: str):
+    df = df.copy()
+
+    df = df.drop_duplicates(subset=col_chave, ignore_index=True)
+
+    df = df.replace(to_replace={"": None, pd.NA: None, np.nan: None})
+
+    for col in df.columns:
+        if "email" in col:
+            df[col] = list(
+                map(lambda email: tratar_emails(email) if email else None, df[col])
             )
 
-            COLS_EMAIL = [
-                'conv_exames_emails',
-                'exames_realizados_emails',
-                'absenteismo_emails'
-            ]
+    df["data_alteracao"] = datetime.now(tz=TIMEZONE_SAO_PAULO)
+    df["alterado_por"] = curr_username
 
-            COLS_BOOL = [
-                'conv_exames',
-                'exames_realizados',
-                'absenteismo',
-            ]
-
-            tabela = int(form.tabela.data)
-            match tabela:
-                case 1: # Empresas
-                    tabela = Empresa
-                    coluna_chave = 'id_empresa'
-
-                    # checar colunas
-                    colunas_erro = ''
-                    for col in df.columns:
-                        if col not in COLS_BOOL + COLS_EMAIL + [coluna_chave]:
-                            colunas_erro = colunas_erro + str(col)
-                    if colunas_erro:
-                        flash(f'Colunas erradas: {colunas_erro}', 'alert-danger')
-                        return redirect(url_for('config_relatorios_agendados'))
-
-                    modelos_query = [
-                        (Empresa.id_empresa),
-                        (Empresa.razao_social),
-                        (Empresa.conv_exames),
-                        (Empresa.conv_exames_emails),
-                        (Empresa.exames_realizados),
-                        (Empresa.exames_realizados_emails),
-                        (Empresa.absenteismo),
-                        (Empresa.absenteismo_emails)
-                    ]
-
-                    filtros = [Empresa.id_empresa.in_(df[coluna_chave].drop_duplicates().astype(int))]
-
-                case 2: # Unidades
-                    tabela = Unidade
-                    coluna_chave = 'id_unidade'
-
-                    # checar colunas
-                    colunas_erro = ''
-                    for col in df.columns:
-                        if col not in COLS_BOOL + COLS_EMAIL + [coluna_chave]:
-                            colunas_erro = colunas_erro + str(col)
-                    if colunas_erro:
-                        flash(f'Colunas erradas: {colunas_erro}', 'alert-danger')
-                        return redirect(url_for('config_relatorios_agendados'))
-
-                    modelos_query = [
-                        (Unidade.id_unidade), # inserir nome da empresa
-                        (Unidade.nome_unidade),
-                        (Unidade.conv_exames),
-                        (Unidade.conv_exames_emails),
-                        (Unidade.exames_realizados),
-                        (Unidade.exames_realizados_emails),
-                        (Unidade.absenteismo),
-                        (Unidade.absenteismo_emails)
-                    ]
-
-                    filtros = [Unidade.id_unidade.in_(df[coluna_chave].drop_duplicates().astype(int))]
-            
-            # tratar df
-            df.drop_duplicates(subset=coluna_chave, ignore_index=True, inplace=True)
-            df = df.replace(to_replace={'': None, pd.NA: None, np.nan: None})
-
-            for col in df.columns:
-                if 'email' in col:
-                    df[col] = list(map(tratar_emails, df[col]))
-
-            df['data_alteracao'] = dt.datetime.now(tz=TIMEZONE_SAO_PAULO)
-            df['alterado_por'] = current_user.username
-            df = df.to_dict(orient='records')
-            if form.celulas_null.data:
-                # se celulas null for True, manter as celulas vazias para \
-                # que a informacao correspondente seja excluida na tabela da database
-                df_final = df
-            else:
-                # se for False, eliminar as celulas vazias para que a informacao \
-                # nao seja apagada na database
-                df_final = []
-                for dicionario in df:
-                    df_final.append({chave: valor for chave, valor in dicionario.items() if valor is not None})
-
-            # backup antes de atualizar
-            timestamp = int(dt.datetime.now().timestamp())
-            query_backup = (
-                database.session.query(*modelos_query)
-                .filter(*filtros)
-            )
-            df_backup = pd.read_sql(query_backup.statement, con=database.session.bind)
-            df_backup.to_csv(
-                path_or_buf=f'{UPLOAD_FOLDER}/backup_{timestamp}.csv',
-                index=False,
-                sep=';',
-                encoding='iso-8859-1'
-            )
-
-            # atualizar
-            database.session.bulk_update_mappings(tabela, df_final)
-            database.session.commit()
-
-            # gerar report das atualizacoes
-            query_alteracoes = (
-                database.session.query(*modelos_query)
-                .filter(*filtros)
-            )
-            df_alteracoes = pd.read_sql(query_alteracoes.statement, con=database.session.bind)
-            df_alteracoes.to_csv(
-                path_or_buf=f'{UPLOAD_FOLDER}/alteracoes_{timestamp}.csv',
-                index=False,
-                sep=';',
-                encoding='iso-8859-1'
-            )
-
-            caminho_arquivos = zipar_arquivos(
-                caminhos_arquivos=[
-                    f'{UPLOAD_FOLDER}/alteracoes_{timestamp}.csv',
-                    f'{UPLOAD_FOLDER}/backup_{timestamp}.csv'
-                ],
-                caminho_pasta_zip=f'{UPLOAD_FOLDER}/report_{timestamp}.zip'
-            ).split('/')[-1]
-            
-
-            return send_from_directory(directory=UPLOAD_FOLDER, path='/', filename=caminho_arquivos)
-
-    return render_template('configs_relatorios_agendados.html', title='GRS+Connect', form=form)
+    return df
 
 
-@app.route('/relatorios_agendados/modelo_empresas', methods=['GET', 'POST'])
+def __gerar_arq_estado_atual(
+    nome: str, cols_query: list[Any], filtros_query: list[Any]
+):
+    """
+    Realiza query para mostrar como a tabela está atualmente.
+
+    Retorna caminho do csv gerado a partir da query
+    """
+    query = db.session.query(*cols_query).filter(*filtros_query)  # type: ignore
+
+    timestamp = int(datetime.now().timestamp())
+    nome_arq = os.path.join(UPLOAD_FOLDER, f"{nome}_{timestamp}.csv")
+
+    df = pd.read_sql(query.statement, con=db.session.bind)  # type: ignore
+
+    df.to_csv(
+        path_or_buf=nome_arq,
+        index=False,
+        sep=";",
+        encoding="iso-8859-1",
+    )
+
+    return nome_arq
+
+
+def __handle_celulas_null(
+    df_mapping: list[dict[str, Any]], manter_celulas_null: bool = False
+):
+    """
+    Se celulas null for True, manter as celulas vazias para
+    que a informacao correspondente SEJA excluida na tabela da db.
+
+    Se for False, eliminar as celulas vazias para que a informacao
+    NAO SEJA apagada na db
+    """
+    if manter_celulas_null is True:
+        final_mapping = df_mapping
+    elif manter_celulas_null is False:
+        final_mapping = []
+        for dic in df_mapping:
+            aux = {k: v for k, v in dic.items() if v is not None}
+            final_mapping.append(aux)
+    else:
+        raise ValueError("manter_celulas_null deve ser booleano")
+    return final_mapping
+
+
+@rel_agendados_bp.route("/modelo-empresas", methods=["GET", "POST"])
 @login_required
 @admin_required
-def relatorios_agendados_modelo_empresas():
-    colunas = [
-        'id_empresa',
-        'conv_exames',
-        'conv_exames_emails',
-        'exames_realizados',
-        'exames_realizados_emails',
-        'absenteismo',
-        'absenteismo_emails'
+def rel_agendados_modelo_empresas():
+    COLS = [
+        "id_empresa",
+        "conv_exames",
+        "conv_exames_emails",
+        "exames_realizados",
+        "exames_realizados_emails",
+        "absenteismo",
+        "absenteismo_emails",
     ]
 
-    df = pd.DataFrame(columns=colunas)
+    df = pd.DataFrame(columns=COLS)
 
-    # criar arquivo dentro da pasta
+    caminho_arq = os.path.join(UPLOAD_FOLDER, "Relatorios_Agendados_Empresas.csv")
+
     df.to_csv(
-        f'{UPLOAD_FOLDER}/Relatorios_Agendados_Empresas.csv',
-        sep=';',
+        path_or_buf=caminho_arq,
+        sep=";",
         index=False,
-        encoding='iso-8859-1'
+        encoding="iso-8859-1",
     )
-    return send_from_directory(directory=UPLOAD_FOLDER, path='/', filename='Relatorios_Agendados_Empresas.csv')
+
+    return send_from_directory(
+        directory=UPLOAD_FOLDER, path="/", filename=os.path.basename(caminho_arq)
+    )
 
 
-@app.route('/relatorios_agendados/modelo_unidades', methods=['GET', 'POST'])
+@rel_agendados_bp.route("/modelo-unidades", methods=["GET", "POST"])
 @login_required
 @admin_required
-def relatorios_agendados_modelo_unidades():
-    colunas = [
-        'id_unidade',
-        'conv_exames',
-        'conv_exames_emails',
-        'exames_realizados',
-        'exames_realizados_emails',
-        'absenteismo',
-        'absenteismo_emails'
+def rel_agendados_modelo_unidades():
+    COLS = [
+        "id_unidade",
+        "conv_exames",
+        "conv_exames_emails",
+        "exames_realizados",
+        "exames_realizados_emails",
+        "absenteismo",
+        "absenteismo_emails",
     ]
 
-    df = pd.DataFrame(columns=colunas)
+    df = pd.DataFrame(columns=COLS)
 
-    # criar arquivo dentro da pasta
+    caminho_arq = os.path.join(UPLOAD_FOLDER, "Relatorios_Agendados_Unidades.csv")
+
     df.to_csv(
-        f'{UPLOAD_FOLDER}/Relatorios_Agendados_Unidades.csv',
-        sep=';',
+        path_or_buf=caminho_arq,
+        sep=";",
         index=False,
-        encoding='iso-8859-1'
+        encoding="iso-8859-1",
     )
-    return send_from_directory(directory=UPLOAD_FOLDER, path='/', filename='Relatorios_Agendados_Unidades.csv')
 
+    return send_from_directory(
+        directory=UPLOAD_FOLDER, path="/", filename=os.path.basename(caminho_arq)
+    )
